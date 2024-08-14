19d5bc28584e72f882c086862d01fe5d744e02ba
mandarmahajan3 <mandar.mahajan@wipro.com>
2024-08-14 10:43:07 +0530

    GKE Code Update

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -42,7 +42,7 @@ resource "google_container_cluster" "primary" {
 
   private_cluster_config {
     enable_private_nodes    = true
-    enable_private_endpoint = true
+    enable_private_endpoint = false
     master_ipv4_cidr_block  = var.master_ipv4_cidr_block
   }
 }
