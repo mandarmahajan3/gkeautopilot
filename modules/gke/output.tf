4c23e70afc3ed19786c1893b1290727d79779f9c
mandarmahajan3 <mandar.mahajan@wipro.com>
2024-08-14 15:39:32 +0530

    GKE Code Update

--- modules/gke/output.tf
+++ modules/gke/output.tf
@@ -1,30 +1,28 @@
+output "kubeconfig" {
+  value = google_container_cluster.primary.endpoint != "" ? join("\n", [
+    "apiVersion: v1",
+    "kind: Config",
+    "clusters:",
+    "- cluster:",
+    "    certificate-authority-data: ${base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)}",
+    "    server: https://${google_container_cluster.primary.endpoint}",
+    "  name: ${google_container_cluster.primary.name}",
+    "contexts:",
+    "- context:",
+    "    cluster: ${google_container_cluster.primary.name}",
+    "    user: ${google_container_cluster.primary.name}",
+    "  name: ${google_container_cluster.primary.name}",
+    "current-context: ${google_container_cluster.primary.name}",
+    "users:",
+    "- name: ${google_container_cluster.primary.name}",
+    "  user:",
+    "    token: ${data.google_client_config.default.access_token}"
+  ]) : "Cluster not created yet"
+}
+
 output "cluster_name" {
   value = google_container_cluster.primary.name
 }
-#
-output "kubeconfig" {
-  value = <<EOT
-apiVersion: v1
-kind: Config
-clusters:
-- cluster:
-    certificate-authority-data: ${base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)}
-    server: https://${google_container_cluster.primary.endpoint}
-  name: ${google_container_cluster.primary.name}
-contexts:
-- context:
-    cluster: ${google_container_cluster.primary.name}
-    user: ${google_container_cluster.primary.name}
-  name: ${google_container_cluster.primary.name}
-current-context: ${google_container_cluster.primary.name}
-users:
-- name: ${google_container_cluster.primary.name}
-  user:
-    token: ${data.google_client_config.default.access_token}
-EOT
-}
-
-
 
 output "cluster_location" {
   value = google_container_cluster.primary.location
