523d529b27970362dfb154bcbe3b69762ea74a46
mandarmahajan3 <mandar.mahajan@wipro.com>
2024-08-20 16:54:24 +0530

    GKE Code Update

--- environments/non-prod-non-phi/gke.tf
+++ environments/non-prod-non-phi/gke.tf
@@ -1,5 +1,4 @@
 
-
 # Define the GKE module
 /*
 module "gke" {
@@ -48,21 +47,3 @@ module "artifact_registry" {
 
 */
 # Main Module
-# Main Module
-
-module "cloudsql_instance" {
-  source             = "../../modules/cloudsql_instance"
-  instance_name      = var.instance_name
-  region             = var.region
-  db_version         = var.db_version
-  project_id         = var.project_id
-  db_tier            = var.db_tier
-  allowed_consumer_projects = var.allowed_consumer_projects
-}
-
-module "cloudsql_database" {
-  source             = "../../modules/cloudsql_database"
-  database_name      = var.database_name # Passing database_name variable
-  instance_name      = module.cloudsql_instance.instance_name
-  db_username        = var.db_username # Passing db_username variable
-}
