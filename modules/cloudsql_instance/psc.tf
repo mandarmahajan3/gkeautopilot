fe5e1fd7c15fc30093cbb2e09ecf09ff78e7c691
mandarmahajan3 <mandar.mahajan@wipro.com>
2024-08-21 16:27:39 +0530

    GKE Code Update

--- modules/cloudsql_instance/psc.tf
+++ modules/cloudsql_instance/psc.tf
@@ -1,5 +1,5 @@
 resource "google_compute_address" "default" {
-  name         = "psc-compute-address-${google_sql_database_instance.default}"
+  name         = "psc-compute-address-${var.instance_name}"
   region       = "us-central1"
   address_type = "INTERNAL"
   subnetwork   = "default"     # Replace value with the name of the subnet here.
@@ -12,7 +12,7 @@ data "google_sql_database_instance" "default" {
 }
 
 resource "google_compute_forwarding_rule" "default" {
-  name                  = "psc-forwarding-rule-${google_sql_database_instance.default.name}"
+  name                  = "psc-forwarding-rule-${var.instance_name}"
   region                = "us-central1"
   network               = "default"
   ip_address            = google_compute_address.default.self_link
