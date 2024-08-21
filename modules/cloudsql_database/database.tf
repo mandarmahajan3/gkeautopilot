4738ee93e568c2a05b2a8f4c17b74f3c32d3e863
mandarmahajan3 <mandar.mahajan@wipro.com>
2024-08-21 14:01:47 +0530

    GKE Code Update

--- modules/cloudsql_database/database.tf
+++ modules/cloudsql_database/database.tf
@@ -1,27 +1,28 @@
-#Create a module for single database instance creation. 
-
-# Generate a unique password for each environment
-resource "random_password" "db_password" {
+# Generate unique passwords for each user
+resource "random_password" "master_user_password" {
   length   = 16
   special  = true
 }
 
-# Create the database for each environment
-resource "google_sql_database" "database" {
-  name     = var.database_name
-  instance = var.instance_name
+resource "random_password" "dml_user_password" {
+  length   = 16
+  special  = true
 }
 
-# Create the master user for each database
+resource "random_password" "ddl_user_password" {
+  length   = 16
+  special  = true
+}
+
+# Create the master user for the database
 resource "google_sql_user" "master_user" {
   name     = var.db_username
   instance = var.instance_name
-  password = random_password.db_password.result
+  password = random_password.master_user_password.result
 }
 
 # Store the master user password in GCP Secret Manager
 resource "google_secret_manager_secret" "db_master_secret" {
-
   secret_id = "${var.instance_name}-db-master-password"
   
   replication {
@@ -33,8 +34,40 @@ resource "google_secret_manager_secret" "db_master_secret" {
   }
 }
 
-# Store the password in Secret Manager
+# Store the master user password in Secret Manager
 resource "google_secret_manager_secret_version" "db_master_secret_version" {
    secret      = google_secret_manager_secret.db_master_secret.id
-  secret_data = random_password.db_password.result
+   secret_data = random_password.master_user_password.result
+}
+
+# Create the DML user
+resource "google_sql_user" "dml_user" {
+  name     = "dml_user"
+  instance = var.instance_name
+  password = random_password.dml_user_password.result
+  host     = "%"
+}
+
+# Create the DDL user
+resource "google_sql_user" "ddl_user" {
+  name     = "ddl_user"
+  instance = var.instance_name
+  password = random_password.ddl_user_password.result
+  host     = "%"
+}
+
+# Grant DML and DDL permissions using a local-exec provisioner
+resource "null_resource" "grant_permissions" {
+  provisioner "local-exec" {
+    command = <<EOT
+    mysql -u root -p"${random_password.master_user_password.result}" -h "${google_sql_database_instance.your_instance.public_ip_address}" -D "${var.database_name}" -e "
+    GRANT SELECT, INSERT, UPDATE, DELETE ON ${var.database_name}.* TO 'dml_user'@'%';
+    GRANT CREATE, ALTER, DROP ON ${var.database_name}.* TO 'ddl_user'@'%';"
+    EOT
+  }
+  
+  depends_on = [
+    google_sql_user.dml_user,
+    google_sql_user.ddl_user,
+  ]
 }
